0954ffc4c5c9a71523a0a4fa93fc36d545d76cc9
JEROBAAL-I7\Juan Oliver <acosta.herrera.oliver@gmail.com>
2017-11-02 00:38:12 -0400

    cambios

--- appBase.js
+++ appBase.js
@@ -1,21 +1,157 @@
-function cssCore(){}
-
-
-var css = new cssCore();
-
-
-    var menuClick = (function()
-    {
-        var defaultClass = "w3-green";
-        var last = null;
-        
-            last = document.getElementById("selected");
+// var menuClick = (function()
+// {
+//  var defaultClass = "w3-green";
+//  var last = null;
+//  last = document.getElementById("selected");
     
-        return function(el)
-        {
-            last.classList.remove(defaultClass);
-            el.classList.add(defaultClass);
-            last = el;
-        };
-    
+// return function(el)
+// {
+//  last.classList.remove(defaultClass);
+//  el.classList.add(defaultClass);
+//  last = el;
+// };
+// })();
+
+
+// var listItem = (function ()
+// {
+//     let principal = null;
+//     let childs = null;
+//     let pClass = null;
+//     return function(el, childsEls, theClass){
+//         principal = el;
+//         childs = childsEls;
+//         pClass = theClass;
+//     };
+// })();
+
+function valuesManager(val){
+    let value = null;
+    if(val !== undefined){ value = val;}
+    var values =(function(){
+        return {
+            get: function(){ return value;},
+            set: function(v) { value = v;}
+        }
     })();
+    return values;
+}
+
+
+function listItems(principal, childs, cssClass, elNumber)
+{
+   //let internalValue = new valuesManager();
+   let el;
+   var collection = document.getElementById(principal).getElementsByTagName(childs);
+   if(collection.length > 0){
+       //internalValue.set(collection[elNumber]);
+       collection[elNumber].classList.add(cssClass);
+       el = collection[elNumber];
+       var i = 0;
+       for(;i< collection.length; i++){
+           collection[i].addEventListener("click", function(event){
+               el.classList.remove(cssClass);
+               event.currentTarget.classList.add(cssClass);
+               el = event.currentTarget;
+           });
+       }
+   }
+   
+}
+let menuPrincipal  = new listItems("menu-principal","a","w3-green",0);
+
+
+
+
+var modal  = (function ()
+{
+    function btns( id,  fnParameter)
+    {
+        if(fnParameter !== undefined  && id !== undefined)
+        {
+           let btn =  document.getElementById(id);
+           btn.classList.remove('w3-hide');
+           btn.addEventListener("click", fnParameter);
+       
+        }else if(id !== undefined){
+            document.getElementById(id).classList.add("w3-hide");
+            
+        }
+        
+     
+    }
+    return{
+        setExito: function(titulo, mensaje, pie)
+        {
+            document.getElementById('titulo-modal-exito').innerHTML = titulo;
+            document.getElementById('mensaje-modal-exito').innerHTML = mensaje;
+            document.getElementById('pie-modal-exito').innerHTML = pie;
+        },
+        showExito: function(show)
+        {
+            if(show){
+                document.getElementById('modal-exito').style.display = 'block' ;
+            }else{
+                document.getElementById('modal-exito').style.display = 'none' ;
+            }
+        },
+        setAdventencia: function(titulo, mensaje, pie)
+        {
+            document.getElementById('titulo-modal-advertencia').innerHTML = titulo;
+            document.getElementById('mensaje-modal-advertencia').innerHTML = mensaje;
+            document.getElementById('pie-modal-advertencia').innerHTML = pie;
+        },
+        showAdventencia: function(show)
+        {
+            if(show){
+                document.getElementById('modal-advertencia').style.display = 'block' ;
+            }else{
+                document.getElementById('modal-advertencia').style.display = 'none' ;
+            }
+        },
+        
+        setError: function(titulo, mensaje, pie, fnAceptar, fnCancelar)
+        {
+            document.getElementById('titulo-modal-error').innerHTML = titulo;
+            document.getElementById('mensaje-modal-error').innerHTML = mensaje;
+            document.getElementById('pie-modal-error').innerHTML = pie;
+            btns('btn-modal-error-aceptar', fnAceptar);
+            btns('btn-modal-error-cancelar', fnCancelar);
+        },
+        showError: function(show)
+        {
+            if(show){
+                document.getElementById('modal-errores').style.display = 'block' ;
+            }else{
+                document.getElementById('modal-errores').style.display = 'none' ;
+            }
+        }
+    
+    }
+})();
+
+// let i = 0;
+// var mensaje = "";
+// for (;i<100; i++){
+//   mensaje += "Esto es un vendito mensaje de prueba \n";
+// }
+
+// var add =   (function()
+// {
+//     var i = 0;
+//     return function()
+//     {
+//        return i++;
+//     };
+
+// })();
+// function aceptar()
+// {
+//       console.log(add());
+// }
+
+// function cancelar(){
+// modal.showError(false);
+// }
+// modal.setError("modal para mostrar los errores",mensaje,"",aceptar,cancelar );
+// modal.showError(true);
--- vuecss.js
+++ vuecss.js
@@ -1,36 +1,68 @@
 
-var data = {
-    progressBarData:0
-};
-    Vue.component('progress-bar', {
-        template:`
-        <div class="w3-border">
-        <div v-bind:style="sstyle" v-bind:class="classes">{{percent}}</div>
-        </div>`,
-        data: function()
-        {
-        
-        sstyle = {
-                height : "24px",
-                width  : data.progressBarData + "%"
-        };
-        return {
-            sstyle:sstyle,
-            classes : "w3-green",
-            percent : data.progressBarData 
+  var pb  = new Vue({
+    el: '#sample',
+    data: {
+        color:"w3-green",
+        width:0,
+        style:{
+            width:0,
+            height:"24px",
+            "text-align":"center",
+
         }
+    },
+    methods:{
+        change: function(current){
+            this.width = current + "%";
+            this.style.width = this.width;
         }
+    }
 
-    });
+   
+  });
+  var v = 40;
+  pb.change(v);
+  function count(){
+  setTimeout(function() {
+      v++;
+        pb.change(v);
+      if(v <100){
+        count();
+      }
+  },1000);
+}
+count();
 
+Vue.component('modal',{
+template: `<div class="w3-modal" id="modal-error">
+  <div class="w3-modal-content">
+  <header class="w3-container w3-panel w3-red">
+  <span class="w3-button w3-display-topright" onclick="document.getElementById('modal-error').style.display = 'none'">&times;</span>
+  
+    <h2>Se ha producido un error, por favor contactar con el administrador.</h2>
 
-data.progressBarData  = 22;
-new Vue({
-     el:"#bar1"
-     
+  </header>
+ <div class="w3-container" style="height:200px; overflow-y:auto !important;">
+  <p>{{mensaje}}</p>
+  </div>
+  <footer class="w3-container w3-red">
+   <p></p>
+  </footer>
+  </div>
+</div>`,
+props:['mensaje'],
+
 });
+var data = "";
+var i = 0;
+for (;i<100; i ++)
+{
+    data += "mucho texto que mostrar \n";
+}
 
-new Vue({
-    el:"#bar2"
-    
+var modal =  new Vue({
+    el:"#mymodal",
+    data:{
+        mensaje:data
+    }
 });
